32ca9c7e2619c2a9b90762d793832fba09dac762
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-07-30 11:21:23 -0400

    notes & refactor

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Rules from './components/rules';
 import Graph from './components/graph';
--- gameoflife/src/components/graph.js
+++ gameoflife/src/components/graph.js
@@ -10,14 +10,15 @@ const numCols = 100;
 const generateEmptyGrid = () => {
         const rows = [];
         for (let i = 0; i < numRows; i++) {
+            //push to rows, Array.from ->, fill all ele's in arrays with 0's, to len of numCols
             rows.push(Array.from(Array(numCols), () => 0))
         }
         return rows
 }
 
-function refreshPage() {
-    window.location.reload(false);
-  }
+// function refreshPage() {
+//     window.location.reload(false);
+//   }
 
 const operations = [
     [0, 1],
@@ -31,9 +32,11 @@ const operations = [
   ];
 
 const Graph = () => {
+    // set grid in state to the GenerateEmptyGrid() fn ^^
     const [grid, setGrid] = useState(() => {
         return generateEmptyGrid()
     });
+    console.log("Grid created in state! -> ",grid);
 
     {/* STATE DRIVEN VARIABLES */}
 
@@ -41,11 +44,10 @@ const Graph = () => {
 
     const [seconds, setSeconds] = useState(0)
 
-    const [running, setRunning] = useState(false);
+    const [running, setRunning] = useState(false);  //used for start button after getting grid toggleable
 
 
-    {/* FOR TIMING */}
-
+    // FOR TIMING
     useEffect(() => {
     if (running) {
       const intervalId = window.setInterval(() => {
@@ -55,29 +57,40 @@ const Graph = () => {
         }
     }, [running]);
 
+    // useRef hook. runningRef = current value of running. (true or false)
     const runningRef = useRef(running);
     runningRef.current = running
 
-    {/* RUN SIMULATION FN & NEIGHBOR CHECKER */}
+
+    // to get the start button to work
+    /* RUN SIMULATION FN & NEIGHBOR CHECKER */
 
     const runSimulation = useCallback(() => {
+            //if were not runnng, return.
         if (!runningRef.current){
             return
         }
 
+        //updating [grid, setGrid]
+          // g = current value - on immer produce fn
         setGrid(g => {
             return produce(g, gridCopy => {
+              //gridCopy will be updated now based on..
               for (let i = 0; i < numRows; i++) {
                 for (let k = 0; k < numCols; k++) {
                   let neighbors = 0;
+                  // neighbor position values are operations
+                  // compute the number of neighbors we have
                   operations.forEach(([x, y]) => {
                     const newI = i + x;
                     const newK = k + y;
+                    //make sure we dont go out of bounds
                     if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
                       neighbors += g[newI][newK];
                     }
                   });
-      
+
+                  // then we see what happens to the cell
                   if (neighbors < 2 || neighbors > 3) {
                     gridCopy[i][k] = 0;
                   } else if (g[i][k] === 0 && neighbors === 3) {
@@ -87,7 +100,8 @@ const Graph = () => {
               }
             });
           });
-      
+          
+          // recursively call to our speed
           setTimeout(runSimulation, speed);
           console.log(speed)
         }, []);
@@ -98,12 +112,13 @@ const Graph = () => {
 
 
         <div className="simulationButtons">
-        {/* START SIM*/}
         <button
+        // start sim
             onClick={() => {
             setRunning(!running);
             if(!running){
             runningRef.current = true;
+            //run simulation
             runSimulation()
             }
             
@@ -119,7 +134,7 @@ const Graph = () => {
         }}>
         Clear Simulation </button>
 
-        {/*ADD RESET SPEED!!!!!!!!!!!!!!!!!!!!!!!!!! */}
+        {/* RANDOM SIM*/}
         <button onClick={() => {
             const rows = [];
             for (let i = 0; i < numRows; i++) {
@@ -130,22 +145,34 @@ const Graph = () => {
         }}
         >
         Random Simulation </button>
+
         </div>
 
 
         {/* GRID AND SIMULATION */}
 
         <div className="graphStats">
-        <div className = "simulation" style={{
+        <div className = "simulation"
+            /* Display grid, repeat is column number. 10px too. */
+            style={{
             display:'grid',
             gridTemplateColumns: `repeat(${numCols}, 10px)`
-        }}>
+            }}>
+            {/* Displaying the grid, by grid.map => rows.map
+            get index of row and column, [i][k] if alive.. black
+            if dead..undefined..no color. Using index as key*/}
             {grid.map((rows, i) =>
                 rows.map((col, k) => 
                     <div 
                     key={`${i}-${k}`}
+                    // make each div clickable. Set current index to a 1
+                    // update at position [i][k]
+                    // could use grid[i][k] = 1 - but we use immer-produce()
                     onClick={() => {
+                                      // pass our current grid
+                                      //    and alter the grid copy
                         const newGrid = produce(grid, gridCopy => {
+                                        // toggle at position [i][k]
                             gridCopy[i][k] = grid[i][k] ? 0 : 1;
                         })
                         setGrid(newGrid)
